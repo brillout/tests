855cff69fb8fd125d9519448b79c445e1a295987
Romuald Brillout <github.com@brillout.com>
2019-11-29 15:50:05 +0100

    wip

--- generate_key_times.js
+++ generate_key_times.js
@@ -1,15 +1,22 @@
+const duration_total = 24;
+
+const STEPS = 12;
+
 const val = (
-  new Array(13)
+  new Array(STEPS+1)
   .fill()
   .map((_,i) => {
-    const deviance = (i%2===0?0:0.5)*1/12;
-    const step = i*1/12;
+    const deviance = 0;//(i%2===0?0:0.5)*1/12;
+    const step = i*1/STEPS;
     const val = step + deviance;
+    const duration_step = duration_total/STEPS;
     console.log(JSON.stringify({
       i,
       step,
       deviance,
       val,
+      duration_total,
+      duration_step,
     }, null, 2));
     return val;
   })
