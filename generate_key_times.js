cba7c2327defe2bd2f082f4f1f4fbf703ad651ea
Romuald Brillout <github.com@brillout.com>
2019-11-29 13:24:22 +0100

    wip

--- generate_key_times.js
+++ generate_key_times.js
@@ -1,19 +1,21 @@
-const val = (
-  new Array(14)
-  .fill()
-  .map((_,i) => {
-    const deviance = (i%2===0?0:0.5)*1/13;
-    const step = i/13;
-    const val = step + deviance;
-    console.log(JSON.stringify({
-      i,
-      step,
-      deviance,
-      val,
-    }, null, 2));
-    return val;
-  })
-  .join('; ')
-);
+const assert = require('assert');
 
-console.log(val);
+const keyTimes = [];
+let keyTime = 0;
+
+for(var i=0; i<13; i++) {
+  keyTimes.push(keyTime);
+  const deviance = (i%2===0?0:0.5)*0.7/13;
+  const step = 1/(13+deviance);
+  keyTime = keyTime + step;
+  console.log(JSON.stringify({
+    i,
+    step,
+    deviance,
+    keyTime,
+  }, null, 2));
+}
+
+assert(keyTimes.length===14);
+
+console.log(keyTimes.join('; '));
